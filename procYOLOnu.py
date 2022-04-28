dee0ede46356c865df988e8d6169902c715c373b
JakLemur <44098796+JakLemur@users.noreply.github.com>
2022-04-28 16:45:25 -0700

    BB_Aprl28
    Updates to SORT method

--- procYOLOnu.py
+++ procYOLOnu.py
@@ -17,65 +17,90 @@ import fnmatch
 import scipy.optimize
 from skimage.metrics import structural_similarity as ssim
 import time
-import torch
 from tqdm import tqdm
 
-from classify import SortClassification
 
 
-def analyzeSORT(df, threshold, slow_move, delta_overlap):
+
+
+def analyzeSORT(df,threshold,slow_move,delta_overlap):
     #threshold = 45
     vc = df.label.value_counts()
     test = vc[vc > threshold].index.tolist()
-
-    deadboxes = [] #container for dead worm locations to avoid double calls
-    deathspots = [] #container for location, frame etc of dead worms. returned out of function
-
-    #iterate through individual worms
-    for ID in test:
+    deadboxes = []
+    deathspots = []
+    #slow_move = 3
+    #delta_overlap =0.8    
+    for ID in test:        
+        #print(ID)
         filtval = df['label'] ==ID
         interim = df[filtval]
         interimD = []
         interim2 = np.asarray(interim)
         fill = 0
         deadcount = 0
-
-        #for each observation of an individual worm
+        alivecount = 0
+        #putdeathsite = []
+        #frameNE, x1E, y1E, x2E, y2E, labelE, deltaE, catagoryE, *_ = interim2[0]
         for row in reversed(interim2):
             frameNA, x1A, y1A, x2A, y2A, labelA, deltaA, catagoryA, *_ = row
-            if fill > 1: #only start on second observation
+            if fill > 1:
                 boxA = [x1A, y1A, x2A, y2A]
                 boxB = [x1B, y1B, x2B, y2B]
-                deltaA = bb_intersection_over_union(boxA, boxB) #compare bbox in frame n with n+1
-                #check if boxes overlap enough
+                deltaA = bb_intersection_over_union(boxA, boxB) 
+                #print(deltaA)
                 if deltaA > delta_overlap:
                     deadcount += 1
-                if deadcount == slow_move+1: #if overlap observations has exceeded threshold, record bbox frame and location to export
-                    #check against deadboxes if deadboxes has anything in it and make sure it's not the same worm called multiple times
+                    #print(deadcount)
+                #print(fill)
+                #else:
+                    #deadcount-=1
+               
+                if deadcount == slow_move+1:
                     if deadboxes == []:
-                        deathspots.append([frameNA, x1A, y1A, x2A, y2A,labelA])
-                        deadboxes.append([x1A, y1A, x2A, y2A])
-                        #csv_outputs.append((frameNA/framerate+start_age))
+                        deathspots.append([frameNA, x1A, y1A, x2A, y2A,labelA])     
+                        #putdeathsite.append([frameNA, x1A, y1A, x2A, y2A,labelA])
+                        deadboxes.append([x1A, y1A, x2A, y2A])  
+                        x1Z = x1A
+                        y1Z = y1A
+                        x2Z = x2A
+                        y2Z = y2A
                     else:
                         notunique = 0
                         for box in deadboxes:
                             #print(box)
                             x1D, y1D, x2D, y2D, *_ = box
                             boxD = [x1D, y1D, x2D, y2D]
-                            deltaD = bb_intersection_over_union(boxA, boxD)
-                            if deltaD > 0.3:
+                            deltaD = bb_intersection_over_union(boxA, boxD)  
+                            if deltaD > 0.4:
                                 notunique = 1
                         if notunique == 0:
-                            deathspots.append([frameNA, x1A, y1A, x2A, y2A,labelA])
+                            deathspots.append([frameNA, x1A, y1A, x2A, y2A,labelA])  
                             deadboxes.append([x1A, y1A, x2A, y2A])
-                            #csv_outputs.append((frameNA/framerate+start_age))
-            frameNB, x1B, y1B, x2B, y2B,labelB, deltaB, catagoryB, *_ = row  # set n+1 values to n values and then repeat
-            fill += 1
-            if deadcount == slow_move + 1:  # break out of loop once things have been called enough times
-                break
-    csv_outputs = pd.DataFrame(deathspots, columns=['frame', 'x1', 'y1', 'x2', 'y2', 'label'])
-    return(csv_outputs)
+                            x1Z = x1A
+                            y1Z = y1A
+                            x2Z = x2A
+                            y2Z = y2A
 
+            frameNB, x1B, y1B, x2B, y2B,labelB, deltaB, catagoryB, *_ = row
+            fill +=1
+            if deadcount > slow_move+1:               
+                boxA = [x1A, y1A, x2A, y2A]
+                boxD = [x1Z, y1Z, x2Z, y2Z]
+                deltaD = bb_intersection_over_union(boxA, boxD) 
+                if deltaA < 0.5:
+                    alivecount+=1
+                    if alivecount > 5:
+                        deadcount = 10
+                        deathspots =  deathspots[:-1] 
+                        deadboxes = deadboxes[:-1]
+            if deadcount > 3*slow_move:
+                break
+                
+                
+                
+    csv_outputs = pd.DataFrame(deathspots, columns = ['frame','x1A', 'y1A', 'x2A', 'y2A','labelA'])
+    return(csv_outputs)
 
 def bb_intersection_over_union(boxA, boxB):
     # determine the (x, y)-coordinates of the intersection rectangle
@@ -93,51 +118,60 @@ def bb_intersection_over_union(boxA, boxB):
     # area and dividing it by the sum of prediction + ground-truth
     # areas - the interesection area
     if float(boxAArea + boxBArea - interArea) != 0:
-
+    
         iou = interArea / float(boxAArea + boxBArea - interArea)
     else:
-        iou = 0
+        iou=0
     # return the intersection over union value
     return iou
 
 
+
+
+
+
+
 if __name__ == "__main__":
 
-    CSV_FOLD_PATH = sys.argv[1]  # folder of YOLO outputs
-    VID_FOLD_PATH = sys.argv[2]  # folder of videos
-    OUT_FOLD_PATH = sys.argv[3]
-
-    # ARGS
-    threshold = 75  # number of frames a worm has to be tracked in order to be analyzed
-    slow_move = 20  # number of frames overlapping by 'delta_overlap' before being called dead or paralyzed (15ish=dead,5=paralyzed)
-    delta_overlap = 0.9  # %overlap to be called motionless (.9 for dead, .7 for paralyzed
+    CSV_FOLD_PATH = sys.argv[1] #folder of YOLO outputs
+    OUT_FOLD_PATH = sys.argv[2]
+    
+    #ARGS
+    threshold = 75 #number of frames a worm has to be tracked in order to be analyzed
+    slow_move = 20 #number of frames overlapping by 'delta_overlap' before being called dead or paralyzed (15ish=dead,5=paralyzed)
+    delta_overlap = 0.95 #%overlap to be called motionless (.9 for dead, .7 for paralyzed
+    max_frame = 2750
 
     csv_list = os.listdir(CSV_FOLD_PATH)
     csv_list = list(filter(lambda f: f.endswith('.csv'), csv_list))
 
     print(csv_list)
     csvindex = 0
-    # loop through list of CSVs
-    for csv_name in tqdm(csv_list):
-        csvindex += 1
+    #loop through list of CSVs
+    for csv_name in csv_list:
+        csvindex +=1
+        print("starting", csv_name,"which is ",csvindex,"of",len(csv_list))
         start_time = time.time()
         csv_PATH = os.path.join(CSV_FOLD_PATH, csv_name)
         OUT_PATH = os.path.join(OUT_FOLD_PATH, csv_name)
-
+        
         #read csv and reformat
         df = pd.read_csv(csv_PATH,names=('frame', 'x1', 'y1', 'w','h','label'))
         df['x2']=df[['x1','w']].sum(axis=1)
         df['y2']=df[['y1','h']].sum(axis=1)
         df = df[['frame','x1', 'y1', 'x2', 'y2']]
+        df = df['frame'] < max_frame
+        df = df[filtval]
+        
         unique = df["frame"].unique()
 
-        #initialize sort tracker and create container
-        mot_tracker1 = Sort(max_age=0, min_hits=0, iou_threshold=0.3) #see SORT documentation NEEDS TUNING
+        #initialize sort tracker and create container       
+        mot_tracker1 =Sort(max_age=0, min_hits=0, iou_threshold=0.8)  #see SORT documentation NEEDS TUNING
         sort_outputs = []
+        print("sorting")
         
-
         #sort from end of experiment backwards
-        for x in reversed(unique),total=len(unique):
+        for x in tqdm(reversed(unique),total=len(unique)):
             frame = int(x)
             filtval = df['frame'] == x
             boxes_xyxy = np.asarray(df[filtval])[:,1:5]
@@ -151,9 +185,10 @@ if __name__ == "__main__":
         #create container dataframes for maximum and minimum frames for each label
         dfmin = pd.DataFrame(columns=['frame', 'x1', 'y1','x2','y2','label'])
         dfmax = pd.DataFrame(columns=['frame', 'x1', 'y1','x2','y2','label'])
+        print("creating max/min arrays")
         #for each label, create max and minimum arrays. This is done dumbly feel free to improve via vectorization.
         uniqueTracks = sort_outputs["label"].unique()
-        for track in uniqueTracks:
+        for track in tqdm(uniqueTracks):
             filtval = sort_outputs['label'] == track
             trackdf = sort_outputs[filtval]
             max_value = trackdf['frame'].max()
@@ -164,7 +199,7 @@ if __name__ == "__main__":
             dfmax = dfmax.append(trackmax, ignore_index = True)
 
         #take necessary arrays and reformat all to correct data type (int)
-        #also, remove any SORT label that was observed less than twice. This drastically lowers computation time.
+        #also, remove any SORT label that was observed less than twice. This drastically lowers computation time. 
         #this filter could be removed if the linking step was more improved
         sort2 = sort_outputs
         vc = sort2.label.value_counts()
@@ -178,19 +213,19 @@ if __name__ == "__main__":
         dfmin = dfmin.apply(pd.to_numeric)
         dfmin = dfmin.apply(np.int64)
 
+        print("linking")
+
+    
         
-
-
-
+        pbar = tqdm(total =len(uniqueTracks)+1)
         
-
         #this section is kinda a mess but it works
         #goes from labels at the end of the experiment and progresses toward the beginning
         #for a given label (labelmin)
         #gets the miniumum (earliest) frame it was observed, then loops through the maxiumum frames of other labels
         #if there is sufficient overlap between this min and a max, overwrites the 'label' value in all arrays of the max with the 'label' of the min
-        #if no overlap is found within a threshold of time (2 days curr), break loop and move to next frame
-
+        #if no overlap is found within a threshold of time (2 days curr), break loop and move to next frame 
+        
         #needs tuning and iteration on time and overlap thresholds to figure out optimum
         itersMin = 1
         while itersMin < len(uniqueTracks):
@@ -217,16 +252,17 @@ if __name__ == "__main__":
                 if frameB < frameA and labelA != labelB:
                     boxA = [x1A, y1A, x2A, y2A]
                     boxB = [x1B, y1B, x2B, y2B]
-                    delta = bb_intersection_over_union(boxA, boxB)
+                    delta = bb_intersection_over_union(boxA, boxB)            
                     if delta > 0.2: #overlap threshold
                         sort2 = sort2.replace({'label': labelB}, labelA)
                         dfmax =dfmax.replace({'label': labelB}, labelA)
                         dfmin = dfmin.replace({'label': labelB}, labelA)
                         uniqueTracks[uniqueTracks==labelB]=labelA
-                        break
+                        break            
                     if frameB == (frameA-144): #threshold of time
                         break
-
+            pbar.update(1)
+        pbar.close()
 
 
         #reformat output to be accepted into analyze sort
@@ -234,30 +270,21 @@ if __name__ == "__main__":
         csv_outputs.columns = ['frame', 'x1', 'y1', 'x2', 'y2','label']
         csv_outputs['delta'] = 0
         csv_outputs['catagory'] = 'alive'
-
-        # Sort on backfilled data
-        outputs = analyzeSORT(csv_outputs, threshold, slow_move, delta_overlap)
-        expID = os.path.basename(csv_PATH).strip('.csv')
-
-        # Classification layer of outputs
-        device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        video_path = os.path.join(VID_FOLD_PATH, f"{expID}.avi")
-        if not os.path.exists(video_path):
-            print("Unable to find video file for exp:", expID)
-
-        sort_classifier = SortClassification(outputs, video_path, device)
-        outputs = sort_classifier.update_sort()  # Update outputs with classification
-
-        outputs['expID'] = expID
+        
+        #analyze for death
+        outputs = analyzeSORT(csv_outputs,threshold,slow_move,delta_overlap)
+        outputs['expID'] = os.path.basename(csv_PATH).strip('.csv')
 
         #export and move to next csv file
         pd.DataFrame(outputs).to_csv(OUT_PATH, mode='w', header=True, index=None)
-        #print('finished in:', time.time()-start_time, 'seconds')
-
-
-
-
-
-
-
-
+        print('finished in:',time.time()-start_time,'seconds')   
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
